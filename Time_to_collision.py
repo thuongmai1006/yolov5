babd22b12e6bb392ffbe3ebbeecc4f58ae5d8e03
Thuong Mai <114529976+thuongmai1006@users.noreply.github.com>
2025-04-13 23:43:01 -0700

    Update Time_to_collision.py

--- Time_to_collision.py
+++ Time_to_collision.py
@@ -18,7 +18,15 @@ time.sleep(1)
 model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
 
 # Object class names
-classNames = [...]  # (You can keep your full list here)
+classNames = ['person', 'bicycle', 'car', 'motorbike', 'aeroplane', 'bus', 'train', 'truck', 'boat', 'traffic light',
+    'fire hydrant', 'N/A', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep',
+    'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack', 'umbrella', 'N/A', 'handbag', 'tie',
+    'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard',
+    'surfboard', 'tennis racket', 'bottle', 'N/A', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana',
+    'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch',
+    'potted plant', 'bed', 'dining table', 'N/A', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
+    'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier',
+    'toothbrush']  
 
 # Detection duration tracker
 detection_start = None
